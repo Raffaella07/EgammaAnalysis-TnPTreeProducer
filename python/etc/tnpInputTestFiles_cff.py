e7312a4039efb8085f63c21207555e8600337173
Raffaella07 <raffaella.tram@gmail.com>
2024-12-11 14:17:28 +0100

    change test input files

--- python/etc/tnpInputTestFiles_cff.py
+++ python/etc/tnpInputTestFiles_cff.py
@@ -8,8 +8,8 @@ filesMiniAOD_2018 = {
 }
 
 filesMiniAOD_2022 = {
-    'mc' :   cms.untracked.vstring(''),
-    'data' : cms.untracked.vstring('root://cmsxrootd.fnal.gov//store/data/Run2022B/EGamma/MINIAOD/PromptReco-v1/000/355/558/00000/92629520-53fb-4f1d-8465-a15b5504e149.root'),
+    'mc' :   cms.untracked.vstring('root://cmsxrootd.fnal.gov//store/mc/Run3Summer22EEMiniAODv4/BuToKJPsi_JPsiToEE_SoftQCD_TuneCP5_13p6TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_postEE_v6-v2/60000/14ec21ac-a25b-4499-b31f-536cebea0c95.root'),
+    'data' : cms.untracked.vstring('root://cmsxrootd.fnal.gov//store/mc/Run3Summer22EEMiniAODv4/BuToKJPsi_JPsiToEE_SoftQCD_TuneCP5_13p6TeV_pythia8-evtgen/MINIAODSIM/130X_mcRun3_2022_realistic_postEE_v6-v2/60000/4419ecf7-7341-4ad6-93f4-b81836d720bc.root'),
 }
 
 filesMiniAOD_2023 = {
@@ -24,6 +24,10 @@ filesMiniAOD_2023postBPIX = {
     'mc' : cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/Run3Summer23BPixMiniAODv4/DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/MINIAODSIM/130X_mcRun3_2023_realistic_postBPix_v2-v3/80000/0040c573-d7f9-47c0-bf8a-2c40619c8ffb.root')
     }
 
+filesMiniAOD_2024 = {
+    'mc' : cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/Run3Winter24MiniAOD/DYto2L_M-50_TuneCP5_13p6TeV_pythia8/MINIAODSIM/KeepSi_133X_mcRun3_2024_realistic_v8-v2/2540000/00d74fac-f2a0-48a4-82c3-543b8ec1e324.root'),
+    'data' : cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/data/Run2024A/EGamma0/MINIAOD/PromptReco-v1/000/378/929/00000/f18e336e-86d4-473b-9e54-858bc0c8ad82.root'),
+    }
 filesMiniAOD_2017 = {
     'mc' :   cms.untracked.vstring('file:/eos/cms/store/group/phys_egamma/tnpTuples/testFiles/RunIIFall17MiniAODv2-DYJetsToLL_M-50.root'),
     'data' : cms.untracked.vstring('file:/eos/cms/store/group/phys_egamma/tnpTuples/testFiles/SingleElectron-Run2017B-31Mar2018-v1.root'),
